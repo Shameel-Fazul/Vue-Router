5b6922c9342601dcc51e65f3b0e6db2840618a7c
Shameel Fazul <shameel.fazul@mail.com>
2021-02-24 17:05:59 +0530

    Vue Router
    - Router Linking
    - File Structuring

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium sed accusantium aut, quasi omnis maiores corporis facilis nobis dolores! Dolorem non dignissimos magnam nulla mollitia libero amet debitis ad eveniet sequi excepturi enim voluptatibus voluptatem obcaecati molestias quaerat delectus, molestiae atque pariatur assumenda corporis ipsum quae. Officia quia modi, molestias earum sequi voluptas fuga nulla obcaecati ab est ipsam voluptates rem perferendis, porro at ipsum vel maxime? Ipsum explicabo unde in tempora nostrum placeat, beatae aliquid, ipsa mollitia quidem, aperiam aut nisi voluptate suscipit! Molestias maiores corporis beatae harum dolor facilis eveniet similique laborum quae id, dignissimos quisquam, quod ex!</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium sed accusantium aut, quasi omnis maiores corporis facilis nobis dolores! Dolorem non dignissimos magnam nulla mollitia libero amet debitis ad eveniet sequi excepturi enim voluptatibus voluptatem obcaecati molestias quaerat delectus, molestiae atque pariatur assumenda corporis ipsum quae. Officia quia modi, molestias earum sequi voluptas fuga nulla obcaecati ab est ipsam voluptates rem perferendis, porro at ipsum vel maxime? Ipsum explicabo unde in tempora nostrum placeat, beatae aliquid, ipsa mollitia quidem, aperiam aut nisi voluptate suscipit! Molestias maiores corporis beatae harum dolor facilis eveniet similique laborum quae id, dignissimos quisquam, quod ex!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>
